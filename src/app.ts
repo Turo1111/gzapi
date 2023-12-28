f5f24bb4aaf00558113f69edb63680b723b36276
turo1111 <Matiaszurita1998@gmail.com>
2023-12-28 20:13:29 -0300

    npm build

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import express from 'express'
 import cors from 'cors'
 import { router } from './routes'
 import db from './config/mongo'
-import { init } from './socket';
+import { init } from './socket'
 
 const PORT = process.env.PORT ?? 3001
 const app = express()
@@ -15,9 +15,9 @@ app.use(router)
 db().then(() => console.log('Conexion lista')).catch((e) => console.log('Ocurrio un error en la conexion con la bd', e))
 
 const server = app.listen(PORT, () => {
-	console.log(`Listening on port ${PORT}...`);
-	const io = init(server);
-	io.on('connection', () => {
-		console.log('Client connected!');
-	});
-});
+  console.log(`Listening on port ${PORT}...`)
+  const io = init(server)
+  io.on('connection', () => {
+    console.log('Client connected!')
+  })
+})
