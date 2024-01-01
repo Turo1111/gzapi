27f1be96d83a752190d3ddb69da669d9a7941a7d
turo1111 <Matiaszurita1998@gmail.com>
2024-01-01 18:17:54 -0300

    test socket

--- src/socket.ts
+++ src/socket.ts
@@ -1,37 +1,41 @@
-import { Server } from 'socket.io';
+import { Server } from 'socket.io'
 import {
-	ClientToServerEvents,
-	InterServerEvents,
-	ServerToClientEvents,
-	ServerType,
-	SocketData,
-} from '../src/interfaces/Socket.types';
+  ClientToServerEvents,
+  InterServerEvents,
+  ServerToClientEvents,
+  ServerType,
+  SocketData
+} from '../src/interfaces/Socket.types'
 
-let io: ServerType;
+let io: ServerType
 
-export function init(httpServer: any) {
-	io = new Server<ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData>(httpServer, {
-		// @ts-ignore
-		method: 'GET',
-		cors: {
-			origin: '*',
-		},
-	});
-	return io;
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export function init (httpServer: any) {
+  io = new Server<ClientToServerEvents, ServerToClientEvents, InterServerEvents, SocketData>(httpServer, {
+    // @ts-expect-error
+    method: 'GET',
+    cors: {
+      origin: '*'
+    }
+  })
+  return io
 }
 
-export function getIO(): ServerType {
-	if (!io) {
-		throw new Error('Socket IO not defined!');
-	}
-	return io;
+export function getIO (): ServerType {
+  // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
+  if (!io) {
+    throw new Error('Socket IO not defined!')
+  }
+  return io
 }
 
-export function emitSocket(type: string, params: Object) {
-	try {
-		// @ts-ignore
-		getIO().emit(type, params);
-	} catch (error) {
-		console.log(error);
-	}
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+export function emitSocket (type: string, params: Object) {
+  try {
+    console.log(type, params)
+    // @ts-expect-error
+    getIO().emit(type, params)
+  } catch (error) {
+    console.log(error)
+  }
 }
