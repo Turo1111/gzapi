64b571cbba3e99ca865dab359faa9f57741361f1
turo1111 <Matiaszurita1998@gmail.com>
2024-01-03 22:15:22 -0300

    fix middleware 3/1/24

--- src/middleware/log.ts
+++ src/middleware/log.ts
@@ -3,6 +3,7 @@ import { NextFunction, Request, Response } from 'express'
 const logMiddleware = (req: Request, _: Response, next: NextFunction): void => {
   const header = req.headers
   const userAgent = header['user-agent']
+  console.log('user-agent', userAgent)
   next()
 }
 
