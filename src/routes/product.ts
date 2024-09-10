23dd8d7d9bccac9bdb1fcec311d98ace9ef169b5
turo1111 <Matiaszurita1998@gmail.com>
2024-09-10 16:23:49 -0300

    all product

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -7,8 +7,8 @@ const router = Router()
 
 router.get('/:id', checkJwt, getItem)
 router.get('/', checkJwt, getAllItems)
-router.post('/skip', getItems)
-router.post('/search', getItems)
+router.post('/skip',checkJwt, getItems)
+router.post('/search',checkJwt, getItems)
 router.post('/', checkJwt, postItem)
 /* router.post('/uploadImage', multerMiddleware.single('myfile'), compressImage, uploadImage) */
 router.get('/image/:image', getImage)
